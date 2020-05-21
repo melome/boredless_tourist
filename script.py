a214b22146b67555aa34fe03c0fbb4bfdcc0966b
melome <shanleyrosero@gmail.com>
2020-05-22 01:26:51 +0800

    added test objects

---
+++ script.py
@@ -0,0 +1,6 @@
+#THE BOREDLESS TOURIST
+
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+test_travellers = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+print(destinations)
+
