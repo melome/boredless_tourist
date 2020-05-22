cb7a5e04f4baea4f4d122ae5e1090f1dfb10dff2
melome <shanleyrosero@gmail.com>
2020-05-22 12:39:12 +0800

    created attractions and functionality for adding new attractions

--- script.py
+++ script.py
@@ -1,22 +1,48 @@
 #THE BOREDLESS TOURIST
 
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
-test_traveller = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
-print(destinations)
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
-def get_destination_index(traveler_destination):
-    for index in range(len(destinations)):
+def get_destination_index(destination):
+    """for index in range(len(destinations)):
         if traveler_destination == destinations[index]:
-            destination_index = index
+            destination_index = index"""
+    destination_index = destinations.index(destination)
     return destination_index
 
-print(get_destination_index("Paris, France"))
-
 def get_traveler_location(traveler):
-    traveler_destination = destinations[1]
+    traveler_destination = traveler[1]
     traveler_destination_index = get_destination_index(traveler_destination)
     return traveler_destination_index
 
-test_destination_index = get_traveler_location(test_traveller)
-print(test_destination_index)
+attractions = [[] for attraction in destinations]
+#other way to create empty list
+"""for destination in destinations:
+    attractions.append([])
+print(attractions)"""
+
+
+def add_attraction(destination, attraction):
+    try:
+        destination_index = get_destination_index(destination)
+        attractions_for_destination = attractions[destination_index].append(attraction)
+        return
+    except SyntaxError:
+        return
+
+
+
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+print(attractions)
 
